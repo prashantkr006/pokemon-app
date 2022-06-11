21c4955b5f9b043e30dfe42e7c2f93ec689a64b1
prashantkr006 <prashantkr006@gmail.com>
2022-06-11 15:19:18 +0530

    Updates

---
+++ precache-manifest.54714aeefaf3f78e3bf046c4787cc06f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb222446382bd8721bc237629333d19",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "1e0f5b87ac3cdbac21ad",
+    "url": "/pokemon-app/static/css/main.4ccc25ee.chunk.css"
+  },
+  {
+    "revision": "b61584260b51c0b2a3a6",
+    "url": "/pokemon-app/static/js/2.03cb6ffe.chunk.js"
+  },
+  {
+    "revision": "1e0f5b87ac3cdbac21ad",
+    "url": "/pokemon-app/static/js/main.537760a0.chunk.js"
+  },
+  {
+    "revision": "938d9eb8c41100c8aaac",
+    "url": "/pokemon-app/static/js/runtime~main.6b8623f8.js"
+  }
+]);
